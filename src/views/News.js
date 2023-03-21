10674dbb53a5b691b858215d350fba6755ca759a
arou21 <smarouba2@gmail.com>
2023-03-20 22:05:05 -0600

    changes

--- src/views/News.js
+++ src/views/News.js
@@ -1,39 +1,30 @@
 import { Button } from "@mui/material";
-import { Box, Stack } from "@mui/system";
 import React, { useState, useEffect } from "react";
 import Article from "../component/Article";
 import { apiFetch } from "../fetch";
 
-
 function News() {
   const [newsData, setNewsData] = useState({ news: [] });
 
-  const loadStocks = () => {
-    return apiFetch("api/news")
-      .then((res) => res.json())
-      .then((res) => setNewsData(res))
-      .catch(console.error);
+  const loadNews = () => {
+    fetch("http://localhost:5000/api/news")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Error loading news.");
+        }
+      })
+      .then((data) => setNewsData(data))
+      .catch((error) => console.error(error));
   };
-
-  useEffect(() => {
-    // fetch("http://localhost:5000/api/news", {
-    //   mode: "no-cors",
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => setNewsData(data));
-    loadStocks();
-  }, []);
 
   return (
     <div>
-      <Box xs={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
-        <Box>
-          <h2>News</h2>
-        </Box>
-        <Button variant="contained" xs={{ flexGrow: "none" }} onClick={loadStocks}>
-          refresh
-        </Button>
-      </Box>
+      <h2>News</h2>
+      <Button variant="contained" sx={{ flexGrow: "none" }} onClick={loadNews}>
+        Refresh
+      </Button>
       {newsData.news.map((news) => (
         <div key={news.id}>
           <h2>{news.headline}</h2>
